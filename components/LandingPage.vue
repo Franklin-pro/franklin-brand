566d70d1cad66a1e25e4c120073fe33b47ecdc88
Programmer-works <programmerfranklin@gmail.com>
2024-10-31 12:12:37 +0200

    fix landing page

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,36 +1,63 @@
 <template>
-    <div class="w-full h-full bg-gray-100">
-      <div class="h-screen w-full mx-auto py-20 xl:px-16 xs:px-8 flex md:flex-row xs:flex-col gap-4 justify-center items-center pb-10 pt-4">
-        <div class="w-full">
-          <img class="rounded-full md:max-w-[70%] sm:max-w-[50%] xs:max-w-[60%] mx-auto" src="../assets/WhatsApp Image 2024-10-07 at 21.03.50.jpeg" alt="My Image" />
-        </div>
-        <div class="w-full flex flex-col justify-center gap-4 md:mt-0 sm:mt-8 xs:mt-4">
-          <h1 class="text-4xl font-semibold font-serif ">Hello, I'm Franklin ND.</h1>
-          <h3 class="capitalize text-blue-400">I'm <span class="typing text-green-500 font-bold dark:text-green-700">Fullstack Developer</span></h3>
-          <p>Experienced full-stack developer with 4+ years of expertise in React Js(TS), Nest Js(TS), Nuxt Js(TS), Next Js(TS), Android, and some Flutter experience.</p>
-          <div class="sm:mt-4 xs:mt-2">
-            <button class="px-6 py-3  bg-blue-500 text-white hover:text-blue-500 duration-300 rounded-xl hover:border-2 hover:border-blue-500 hover:bg-transparent">Get In Touch</button>
-          </div>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1
+          class="max-w-2xl mb-4 text-4xl font-black leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+          Franklin <br>ND developer.
+        </h1>
+
+        <h3 class="capitalize text-blue-400">I'm <span
+            class="typing text-green-500 font-bold dark:text-green-700">Fullstack Developer</span></h3>
+        <p>Experienced full-stack developer with 4+ years of expertise in React Js(TS), Nest Js(TS), Nuxt Js(TS), Next
+          Js(TS), Android, and some Flutter experience.</p>
+        <div class="space-y-4 py-5 sm:flex sm:space-y-0 sm:space-x-4">
+
+          <a href="" target="_blank"
+            class="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill-rule="evenodd"
+                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                clip-rule="evenodd"></path>
+            </svg> View on GitHub
+          </a>
+
+          <a href="" target="_blank"
+            class="inline-flex items-center justify-center w-full px-5 py-3 mb-2 mr-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:w-auto focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+
+            <svg class="w-10 h-10" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill-rule="evenodd"
+                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                clip-rule="evenodd"></path>
+            </svg>
+            Get Instagram
+          </a>
+
         </div>
       </div>
+
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <img src="../assets/WhatsApp Image 2024-10-07 at 21.03.50.jpeg" alt="hero image">
+      </div>
+
     </div>
-  </template>
-  
-  <script>
-  
-  export default {
-    mounted() {
-      new Typed('.typing', {
-        strings: ['', 'FullStack Developer,', 'Frontend Developer,', 'Backend Developer,', 'Mobile Apps Developer.'],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-      });
-    }
+  </section>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    new Typed('.typing', {
+      strings: ['', 'FullStack Developer,', 'Frontend Developer,', 'Backend Developer,', 'Mobile Apps Developer.'],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
   }
-  </script>
-  
-  <style scoped>
-  /* Add any specific styling here if needed */
-  </style>
-  
+}
+</script>
+
+<style scoped>
+/* Add any specific styling here if needed */
+</style>
